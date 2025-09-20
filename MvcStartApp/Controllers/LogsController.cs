a1566af206a03d2e9c862710d44eb20a4c51f905
Serega5555 <lazarpalace@gmail.com>
2025-09-20 03:17:49 +0300

    Create LogsController.cs

---
+++ MvcStartApp/Controllers/LogsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcStartApp.Models;
+using MvcStartApp.Repositories;
+using System.Diagnostics;
+
+namespace MvcStartApp.Controllers
+{
+    public class LogsController : Controller
+    {
+        private readonly IRequestRepository _repo;
+
+        public LogsController(IRequestRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var logs = await _repo.GetRequests();
+            return View(logs);
+        }
+    }
+}
