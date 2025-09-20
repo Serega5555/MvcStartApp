949f1df979f2472750d48f585a7739fa276f4834
Serega5555 <lazarpalace@gmail.com>
2025-09-20 03:26:11 +0300

    Create IRequestRepository.cs

---
+++ MvcStartApp/Repositories/IRequestRepository.cs
@@ -0,0 +1,10 @@
+﻿using MvcStartApp.Models.Db;
+
+namespace MvcStartApp.Repositories
+{
+    public interface IRequestRepository
+    {
+        Task AddRequest(DateTime date, HttpContext context);
+        Task<Request[]> GetRequests();
+    }
+}
