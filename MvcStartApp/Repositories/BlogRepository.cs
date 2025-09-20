f7cb30d99d7debce2db7b6d597eb43fb27aaf706
Serega5555 <lazarpalace@gmail.com>
2025-09-20 03:25:22 +0300

    Create BlogRepository.cs

---
+++ MvcStartApp/Repositories/BlogRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcStartApp.Models.Db;
+
+namespace MvcStartApp.Repositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly BlogContext _context;
+
+        public BlogRepository(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUser(User user)
+        {
+            var entry = _context.Entry(user);
+            if (entry.State == EntityState.Detached)
+                await _context.Users.AddAsync(user);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User[]> GetUsers()
+        {
+            return await _context.Users.ToArrayAsync();
+        }
+    }
+}
