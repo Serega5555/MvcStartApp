c0fac0c8396ff77bdc43ddc35ac0d661c6f238b7
Serega5555 <lazarpalace@gmail.com>
2025-09-20 04:33:23 +0300

    Create Startup.cs

---
+++ MvcStartApp/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MvcStartApp.Middlewares;
+using MvcStartApp.Models.Db;
+using MvcStartApp.Repositories;
+
+namespace MvcStartApp
+{
+    public class Startup
+    {
+        IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IBlogRepository, BlogRepository>();
+
+            services.AddScoped<IRequestRepository, RequestRepository>();
+
+            string connection = _configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<BlogContext>(options => options.UseSqlServer(connection), ServiceLifetime.Singleton);
+            
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IRequestRepository repo)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<LoggingMiddleware>(repo);
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
