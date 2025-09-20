60442e08a8c2e8f14482fc9f5cf33e0dbd1b05dc
Serega5555 <lazarpalace@gmail.com>
2025-09-20 03:19:37 +0300

    Create LoggingMiddleware.cs

---
+++ MvcStartApp/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using MvcStartApp.Controllers;
+using MvcStartApp.Repositories;
+using MvcStartApp.Models.Db;
+
+namespace MvcStartApp.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IRequestRepository _repo;
+
+        public LoggingMiddleware(RequestDelegate next, IRequestRepository repo)
+        {
+            _next = next;
+            _repo = repo;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var date = DateTime.Now;
+            
+            Console.WriteLine($"[{date}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+            
+            await _next.Invoke(context);
+
+            await _repo.AddRequest(date, context);
+        }
+    }
+}
